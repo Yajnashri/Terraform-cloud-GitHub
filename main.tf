dfbc624dfd2205daf6b1b4fa73b5970edd86a2c3
Yajnashri <shriyajna@gmail.com>
2025-12-18 15:13:26 +0530

    Update emi

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 	}
 
 resource "aws_instance" "Yajnainfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+	ami = "ami-068c0051b15cdb816" 
 	instance_type = "t3.micro"
     tags = {
     Name = "yajna"
